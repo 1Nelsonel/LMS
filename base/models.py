32d44ce3cb9b03c4eaa4f65e3fe70d31c3e4d8b3
1Nelsonel <nelsonmokoro37@gmail.com>
2022-08-10 10:16:24 +0300

    success

--- base/models.py
+++ base/models.py
@@ -10,35 +10,49 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-# class Room(models.Model):
-#     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-#     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-#     image = models.ImageField(upload_to='static/image', null=True)
-#     name = models.CharField(max_length=200)
-#     description = models.TextField(null=True, blank=True)
-#     participants = models.ManyToManyField(User, related_name='participants', blank=True)
-#     updated = models.DateTimeField(auto_now=True)
-#     created = models.DateTimeField(auto_now_add=True)
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to='image', null=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
-#     class Meta:
-#         ordering = ['-updated', '-created']
+    class Meta:
+        ordering = ['-updated', '-created']
 
-#     def __str__(self):
-#         return self.name[0:50]
+    def __str__(self):
+        return self.name[0:50]
 
+class Curriculum(models.Model):
+    name = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    file = models.FileField(upload_to="files", blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
-# class Message(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     room = models.ForeignKey(Room, on_delete=models.CASCADE)
-#     body = models.TextField()
-#     updated = models.DateTimeField(auto_now=True)
-#     created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-updated', '-created']
 
-#     class Meta:
-#         ordering = ['-updated', '-created']
+    def __str__(self):
+        return self.name[0:50]
+    
 
-#     def __str__(self):
-#         return self.body[0:50]
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.body[0:50]
 
 class Blog(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
